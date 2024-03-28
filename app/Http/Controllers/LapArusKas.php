20f56fae7145bf7c180f8e5a1c165284ad299b2f
Bagas Seno Pamukti <bagassenop4@gmail.com>
2024-03-28 10:21:00 +0700

    edit exportpdf

--- app/Http/Controllers/LapArusKas.php
+++ app/Http/Controllers/LapArusKas.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Akun;
-use App\Models\LabaRugi;
+use App\Models\User;
 use App\Models\Neraca;
+use App\Models\LabaRugi;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf as PDF;
 
@@ -35,10 +36,10 @@ class LapArusKas extends Controller
         return view('konten.lap_arus_kas', compact('dataNeracaNow', 'dataNeracaThen', 'year', 'dataLabarugi', 'month'));
     }
 
-
     public function indexAfterFilterMonthly(Request $request)
     {
         // dd($request->year, ($request->year + 1));
+        // $then = null;
 
         $dataNeracaNow = Neraca::where('periode', $request->year)->where('bulan', $request->month)->get();
         $yearThen = $request->year;
@@ -79,15 +80,30 @@ class LapArusKas extends Controller
         return view('konten.lap_arus_kas_monthly', compact('dataNeracaNow', 'dataNeracaThen', 'year', 'dataLabarugi', 'month', 'then'));
     }
 
+    private function getDataNeracaThen($year, $then)
+    {
+        return Neraca::where('periode', $year)->where('bulan', $then)->get();
+    }
+
     public function exportpdf(Request $request)
     {
-        $dataLapArusKas = $this->indexAfterFilterMonthly($request); // No need for 'Request ($request)'
-        view()->share('data', $dataLapArusKas);
-        $pdf = PDF::loadview('data', ['data' => $dataLapArusKas]); // Pass data as an associative array
-        return $pdf->download('data.pdf');
+        // return $this ->dataNeracaThen($request)->exportpdf();
+        // $data = $this->indexAfterFilterMonthly($request);
+        // $then = $data['then']; // Retrieve $then from $data array
+        // view()->share('data', $data);
+        // $pdf = PDF::loadview('lap-arus-kas-pdf', $data);
+        // return $pdf->download('lap-arus-kas.pdf');
+        // $dataNeracaThen = $this->getDataNeracaThen($request->year, $then); // No need for 'Request ($request)'
+        // // view()->share('data', $dataLapArusKas);
+        // $pdf = PDF::loadview('konten.lap_arus_kas_monthly', $dataNeracaThen); // Pass data as an associative array
+        // return $pdf->download('lap-arus-kas.pdf');
         // $dataLapArusKas = $this->indexAfterFilterMonthly(Request ($request));
         // view()->share('data', $dataLapArusKas);
         // $pdf = PDF::loadview('data', compact($dataLapArusKas));
         // return $pdf->download('data.pdf');
+        $data = LabaRugi::all();
+        view()->share('data', $data);
+        $pdf = PDF::loadView('lap-arus-kas-pdf');
+        return $pdf->download('lap-arus-kas.pdf');
     }
 }
